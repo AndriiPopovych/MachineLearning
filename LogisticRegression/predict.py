c09fb9360df9aa8a78edbb0c0c4638c14e30da3a
Андрій <gunter0104@gmail.com>
2015-11-10 20:58:52 +0200

    Modify predict function. Result this algorithm - 15 true answer/ 5 false answer

--- LogisticRegression/predict.py
+++ LogisticRegression/predict.py
@@ -3,14 +3,14 @@ from train import meanNormalization
 import math
 
 
-def predict(_xArray):
+def calculate(_xArray):
     tettaData = getFileContents("tetta.csv")
     meanCoefficients = getFileJSONContents("meanCoefficients.json")
     sDeviation = meanCoefficients[0]
     meanValue = meanCoefficients[1]
     tetta = tettaData.split(",")
-    xArray = [(1.0 - float(sDeviation[0])) / float(meanValue[0])]
-    i = 1
+    xArray = [] #[(1.0 - float(sDeviation[0])) / float(meanValue[0])]
+    i = 0
     for x in _xArray:
         xArray.append((float(x) - float(sDeviation[i])) / float(meanValue[i]))
         i += 1
@@ -21,4 +21,21 @@ def predict(_xArray):
         i += 1
     result = 1 / (1 + math.exp(-summ))
     return result
+
+
+def predict():
+    xArray, yArray = getDataFromCSV("predict.csv")
+    i = 0
+    trueValue = 0
+    falseValue = 0
+    for x in xArray:
+        predictResult = calculate(x)
+        predictResult = round(predictResult)
+        if predictResult == yArray[i]:
+            trueValue += 1
+        else:
+            falseValue += 1
+        i += 1
+    print "True/False = " + str(trueValue) + "/" + str(falseValue)
+
 
